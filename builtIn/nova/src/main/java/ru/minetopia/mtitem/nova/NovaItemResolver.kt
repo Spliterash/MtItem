4b43bf5605ec7112e44ded2d76b32a79f128e950
MelonHell <MelonHell@ya.ru>
2023-06-12 14:55:07 +0300

    Nova support

---
+++ builtIn/nova/src/main/java/ru/minetopia/mtitem/nova/NovaItemResolver.kt
@@ -0,0 +1,21 @@
+package ru.minetopia.mtitem.nova
+
+import org.bukkit.inventory.ItemStack
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
+import org.springframework.stereotype.Component
+import ru.minetopia.mtitem.api.exceptions.MtItemNotFoundException
+import ru.minetopia.mtitem.api.resolver.ItemResolver
+import xyz.xenondevs.nova.api.Nova
+
+@Component
+@ConditionalOnClass(Nova::class)
+class NovaItemResolver : ItemResolver {
+    override val domain = "nova"
+
+    override fun resolve(text: String): ItemStack {
+        return Nova.getNova().itemRegistry.getOrNull(text)?.createItemStack()
+            ?: throw MtItemNotFoundException(text, this)
+    }
+
+    override fun items(): Collection<String> = listOf()
+}
